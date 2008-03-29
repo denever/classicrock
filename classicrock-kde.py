42d43597de17550beb47219578b449de8b907974
Giuseppe "denever" Martino <denever@autistici.org>
2008-03-29 21:39:34 +0100

    Added timing to both gnome and kde scripts

--- classicrock-kde.py
+++ classicrock-kde.py
@@ -1,18 +1,25 @@
 import os
 import httplib
 import re
+from threading import Timer
 
 find_song = re.compile("^var gimpdata=\"([^~]+)~\d+~([^~]+)~") 
 
-conn = httplib.HTTPConnection("mangle.smgradio.com")
-conn.request("GET","/vc.js?frontpage")
-r1 = conn.getresponse()
-data = str()
-data = r1.read()
-found_song = find_song.search(data)
+def read_song():
+	conn = httplib.HTTPConnection("mangle.smgradio.com")
+	conn.request("GET","/vc.js?frontpage")
+	r1 = conn.getresponse()
+	data = str()
+	data = r1.read()
+	found_song = find_song.search(data)
 
-if found_song:
-	msg = "Title: " + found_song.group(2) +  " Artist: " + found_song.group(1)
-	os.execl("/usb/bin/dcop","dcop","kded","kmilod","displayText","\""+msg+"\"")
-	print error
+	if found_song:
+		msg = found_song.group(1) +  "\n" + found_song.group(2)
+		os.execl("/usb/bin/dcop","dcop","kded","kmilod","displayText","\""+msg+"\"")
+
+if __name__ == "__main__":
+	read_song()
+	t = Timer(120.0,read_song)
+	t.start
+
 	
